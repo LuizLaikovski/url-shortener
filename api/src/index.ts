45729ef12b15738b75838380db6ebcdd1ade0f10
Luiz Laikovski <luizlaikovski@gmail.com>
2026-01-11 15:17:48 -0300

    cors 3

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,18 +3,12 @@ import urlRoutes from './urlRoutes.js';
 import cors from 'cors';
 
 const app = express();
-const PORT: number = 3000;
+const PORT = 3000;
 
-app.use(cors({
-  origin: ["*"],
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "x-api-key"]
-}));
-
-app.options("*", cors());
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.json());
-
 app.use(urlRoutes);
 
 app.listen(PORT, () => {
