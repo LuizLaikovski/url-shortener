f9ed3042c02336c8da1694fede63e6cc8180f271
Luiz Laikovski <luizlaikovski@gmail.com>
2026-01-11 15:10:58 -0300

    arrumando o cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,22 @@
 import express from 'express'
 import urlRoutes from './urlRoutes.js';
+import cors from 'cors';
 
 const app = express();
-const PORT:number = 3000;
+const PORT: number = 3000;
+
+app.use(cors({
+  origin: ["*"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "x-api-key"]
+}));
+
+app.options("*", cors());
 
 app.use(express.json());
 
 app.use(urlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
